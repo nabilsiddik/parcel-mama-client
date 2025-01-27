d29df8ddee07c05c33a7eae261d2ce436c0f8c9d
nabilsiddik <nabilsiddik90@gmail.com>
2025-01-27 18:33:22 +0600

    finished

--- src/Pages/AuthenticationPage/AuthenticationPage.jsx
+++ src/Pages/AuthenticationPage/AuthenticationPage.jsx
@@ -17,12 +17,10 @@ const AuthenticationPage = () => {
   return (
     <div className="bg-gray-100 pt-[180px] pb-[100px] flex justify-center">
       <div className="flex w-full max-w-5xl shadow-lg bg-white rounded-md overflow-hidden">
-        <div className="w-1/2 bg-[#fc0] text-white p-8 flex justify-center items-center">
+        <div className="w-1/2 bg-[#fc0] text-white p-8 justify-center items-center hidden md:flex">
           <img src={deliveryBoy} alt="" />
         </div>
-        <div className="w-1/2 p-8">
-          <div className="flex justify-end">
-          </div>
+        <div className="md:w-1/2 w-full p-8">
           <Card className='py-5'>
             <Outlet/>
           </Card>
